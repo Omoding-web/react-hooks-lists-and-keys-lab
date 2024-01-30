c78bf34289a5cc7e4485120e981eecd3bcea137e
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-01-30 23:42:08 +0300

    Assignment done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ technologies, about, name }) {
   return (
     <div className="project-item">
+      /*To display the name of the project and description */
       <h3>{name}</h3>
       <p>{about}</p>
+      /*Rendering the list of technologies */
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        /*Mapping over the technologies to create spans for each technology */
+        {technologies.map((technology, props) => (
+          <span key={props}>{technology}</span>
+        ))}
+   
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,20 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+{/*Mapping over the projects array to create a ProjectItem component for each project*/}
+  const ProjectItems = projects.map((project) => (
+  <ProjectItem
+  /*Using the id of each project as the key prop*/
+  key={project.id}
+  /*Passing the name, description, and technologies arrays of the project as a prop */
+  name={project.name}
+  about={project.about}
+  technologies={project.technologies} />
+));
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ProjectItems}</div>
     </div>
   );
 }
